8b9b4560a04814aeefef6e16020ec74bd904947c
Elizabeth Excell <bethexcell@gmail.com>
2015-07-19 13:31:03 +0100

    Added j varable

--- firstprogram.js
+++ firstprogram.js
@@ -3,7 +3,12 @@
  */
 
 
+// var i,j;
+j=0;
 for (i = 0; i < 10; i++) {
-    console.log('Hello World!: '+i);
+ //   console.log('Hello World!: '+i);
+    j = j + i;
+    console.log("Loop iteration:"+i+" j: "+j);
+
 }
 
